6ffbe4e78c52cd36020704e111f5f0d3c50618a7
ConwayJ18 <conway.jess18@gmail.com>
2019-03-26 17:03:55 -0500

    Added "scoreboxes" to window. Non-editable and non-functional.

--- src/com/scorekeeper/model/graphics/GraphicsImpl.java
+++ src/com/scorekeeper/model/graphics/GraphicsImpl.java
@@ -2,33 +2,38 @@ package com.scorekeeper.model.graphics;
 
 import java.awt.BorderLayout;
 import java.awt.GraphicsConfiguration;
+import java.awt.TextField;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class GraphicsImpl implements Graphics
 {
-	static GraphicsConfiguration gc;
 	public void start()
 	{
-		JFrame frame= new JFrame(gc);	
-		frame.setTitle("Quidditch Scorekeeper");
-		frame.setSize(1450, 800);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false);
+		JFrame frame= new JFrame("Quidditch Scorekeeper"); //Create window with title "Quidditch Scorekeeper"
+		frame.setSize(1450, 800); //Set window size
 		
-		JButton button,button1, button2, button3,button4;
-		button = new JButton("left");
-		button1 = new JButton("right");
-		button2 = new JButton("top");
-		button3 = new JButton("bottom");
-		button4 = new JButton("center");
-		frame.add(button, BorderLayout.WEST);
-		frame.add(button1, BorderLayout.EAST);
-		frame.add(button2, BorderLayout.NORTH);
-		frame.add(button3, BorderLayout.SOUTH);
-		frame.add(button4, BorderLayout.CENTER);
+		//Create scoreboxes
+		final JTextField scorebox1=new JTextField("0"); //Default value is 0
+		scorebox1.setBounds(25, 200, 675, 400); //Sets location & size
+		scorebox1.setEditable(false); //You cannot type into this box
+		scorebox1.setHorizontalAlignment(JTextField.CENTER); //Center the text in the box
+	    final JTextField scorebox2=new JTextField("0"); //Default value is 0
+		scorebox2.setBounds(750, 200, 675, 400); //Sets location & size
+		scorebox2.setEditable(false); //You cannot type into this box
+		scorebox2.setHorizontalAlignment(JTextField.CENTER); //Center the text in the box
+		
+		//Add scoreboxes to window
+	    frame.add(scorebox1);
+	    frame.add(scorebox2);
+	    
+	    //These things need to go last to prevent loading lag
+		frame.setLocationRelativeTo(null); //Centers window on screen
+		frame.setLayout(null); //Allows us to manually set item locations
+		frame.setResizable(false); //Disallows resizing the window
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Makes program quit when the window closes
+		frame.setVisible(true); //Makes the window visible
 	}
 }
